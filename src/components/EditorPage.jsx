794dbc96661f485ac2975adc4c0fd0ef402a8550
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-02-24 14:08:23 +0100

    create: Editor Page

---
+++ src/components/EditorPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function EditorPage () {
+  return (
+    <div>
+      <h1>Esto es la Editor page</h1>
+      <Link to='/'>Ir a Home</Link>
+
+    </div>
+  )
+}
+
+export default EditorPage
