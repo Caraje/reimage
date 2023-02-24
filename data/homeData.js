8a58d25e95db1f8726365fc83822af35e9aeb0dc
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-02-24 17:43:43 +0100

    separate info of Home to other File

---
+++ data/homeData.js
@@ -0,0 +1,22 @@
+
+export const homeData = [
+  {
+    id: '01',
+    title: 'Mejore sus Imagenes',
+    text: 'Abre tu imagen y prueba los diferentes ajustes que te ofrecemos para mejorar los colores, el contraste o el brillo de tus fotos.',
+    img: './static_img/colors_img.webp'
+  },
+  {
+    id: '02',
+    title: 'Ajuste el tamaño',
+    text: 'Pruebe a usar las opciones para escalar o ajustar sus imagenes, para que siempre tengan el tamaño y las proporciones correctas.',
+    img: './static_img/scale_img.webp'
+  },
+  {
+    id: '03',
+    title: 'Elimine los fondos',
+    text: 'Seleccione una foto para poder eliminar el fondo por completo de manera sencilla y rapida, para poder usarla en sus proyectos.',
+    img: './static_img/bg.webp'
+  }
+]
+//
