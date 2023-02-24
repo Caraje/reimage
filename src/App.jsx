ebdd4c4ef5c0effdad955608a0ccf9c660634e8b
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-02-24 14:07:30 +0100

    add: routes app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-function App () {
-  const [count, setCount] = useState(0)
+import { Route, Routes } from 'react-router-dom'
+import EditorPage from './components/EditorPage'
+import HomePage from './components/HomePage'
 
+function App () {
   return (
-    <>
-    <div className='min-h-screen flex flex-col bg-slate-900'>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-        
-        <footer className='bg-orange-400  mt-auto'>Esto es el pie de pagina</footer>
+    <div className='flex flex-col bg-slate-800 min-h-screen text-white font-nunito'>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/editor' element={<EditorPage />} />
+      </Routes>
     </div>
-    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
-// import './styles/globals.css'
+import './styles/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
