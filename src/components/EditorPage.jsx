fe52c3ed60941ed4d055a444e5592ff7aa1b0118
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-02-24 18:07:22 +0100

    create: editor Page

--- src/components/EditorPage.jsx
+++ src/components/EditorPage.jsx
@@ -3,11 +3,21 @@ import { Link } from 'react-router-dom'
 
 function EditorPage () {
   return (
-    <div>
-      <h1>Esto es la Editor page</h1>
-      <Link to='/'>Ir a Home</Link>
-
-    </div>
+    <>
+      <main className='flex flex-col gap-8 align-middle max-w-full justify-center items-center h-screen bg-gradient-to-br from-[#00cc99]  to-[#6600ff] ' />
+      <footer className='bg-[#fbed21] h-12 flex flex-col items-center justify-center'>
+        <p className='text-black text-center'>App creada por <a href='https://www.carlosajenjo.es/' target='_blank' rel='noreferrer'>Carlos Ajenjo </a></p>
+      </footer>
+      <Link
+        className='
+          absolute top-5 left-5 bg-[#fbed21] w-10 h-10 pt-1 rounded-3xl text-center text-black text-2xl font-bold transition-all
+          hover:bg-[#fff27b] hover:scale-125
+          '
+        to='/'
+      >
+        X
+      </Link>
+    </>
   )
 }
 
