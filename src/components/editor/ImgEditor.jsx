c0c8bd0106c23796d7e3e1f188949a80d6c7bcc5
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-02-27 18:27:14 +0100

    create: view image secction

---
+++ src/components/editor/ImgEditor.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import 'two-up-element'
+
+const ImgEditor = ({ img, editedImg }) => {
+  return (
+    <>
+      <two-up className='flex justify-center items-center'>
+        <div className='max-h-[50rem] max-w-[70rem]'>
+
+          <img src={img} className='h-full w-full  object-contain object-center' />
+        </div>
+        {/* <img src={editedImg} className='max-h-[50rem] max-w-[70rem] object-fill object-center' /> */}
+        <div className='max-h-[50rem] max-w-[70rem]'>
+
+          <img src={editedImg} className='h-full w-full object-contain object-center' />
+        </div>
+      </two-up>
+    </>
+
+  )
+}
+
+export default ImgEditor
