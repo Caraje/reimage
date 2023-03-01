92389244090b2e01bca191086b67f5dcedc03165
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-03-01 17:37:34 +0100

    add: handle error in remove Background

--- src/components/editor/ImgEditor.jsx
+++ src/components/editor/ImgEditor.jsx
@@ -1,6 +1,9 @@
 import 'two-up-element'
 
-const ImgEditor = ({ img, editedImg }) => {
+const ImgEditor = ({ img, editedImg, setError }) => {
+  const handleError = () => {
+    setError(true)
+  }
   return (
     <>
       <two-up className='flex justify-center items-center'>
@@ -12,6 +15,7 @@ const ImgEditor = ({ img, editedImg }) => {
         <div className='max-h-[50rem] max-w-[70rem]'>
           <img
             src={editedImg}
+            onError={handleError}
             className='h-full w-full object-contain object-center'
           />
         </div>
