96595664698cca17faaff17c7753ea39b2a6b674
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-03-02 12:45:41 +0100

    create: component footerWeb

---
+++ src/components/UI/FooterWeb.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FooterWeb = () => {
+  return (
+    <footer className='bg-[#fbed21] h-12 flex flex-col items-center justify-center'>
+      <p className='text-black text-center'>App creada por <a href='https://www.carlosajenjo.es/' target='_blank' rel='noreferrer'>Carlos Ajenjo </a></p>
+    </footer>
+  )
+}
+
+export default FooterWeb
