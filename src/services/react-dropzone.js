0c55cb7c34fe91c31908f260ea240489ffb8a502
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-03-02 20:11:27 +0100

    add: env keys

--- src/services/react-dropzone.js
+++ src/services/react-dropzone.js
@@ -1,13 +1,16 @@
 
 export async function dropzone (files) {
+  const APIKEY_CLOUDINARY = import.meta.env.VITE_APIKEY_CLOUDINARY
+  const NAME_CLOUD_CLOUDINARY = import.meta.env.VITE_NAME_CLOUD_CLOUDINARY
+
   const formData = new FormData()
   formData.append('file', files[0])
   formData.append('upload_preset', 'Reimage')
   formData.append('timestamp', (Date.now() / 1000))
-  formData.append('api_key', '874151438335991')
+  formData.append('api_key', APIKEY_CLOUDINARY)
 
   try {
-    const response = await fetch('https://api.cloudinary.com/v1_1/caraje/image/upload', {
+    const response = await fetch(`https://api.cloudinary.com/v1_1/${NAME_CLOUD_CLOUDINARY}/image/upload`, {
       method: 'POST',
       body: formData
     })
