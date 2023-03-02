7910699ae23f5f5b54b460451c7bcb582070ea70
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-03-02 13:43:35 +0100

    create: editorCanvas Component

---
+++ src/components/editor/EditorCanvas.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ImgEditor from './ImgEditor'
+
+const EditorCanvas = ({ image, originalImg, editedImg, setError }) => {
+  return (
+    <section className={`flex justify-center items-center w-[${image.width}px] max-h-[${image.height}px] bg-gray-700/50 rounded-3xl border-2 border-[#fbed21] p-4`}>
+      {
+      image ? <ImgEditor img={originalImg} editedImg={editedImg} setError={setError} /> : <h2>No hay imagen disponible</h2>
+    }
+    </section>
+  )
+}
+
+export default EditorCanvas
