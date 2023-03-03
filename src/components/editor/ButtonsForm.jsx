7cae49c16e3505c884aac34a782a870e7eda7429
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-03-03 16:04:33 +0100

    add: functional button for download imgs

--- src/components/editor/ButtonsForm.jsx
+++ src/components/editor/ButtonsForm.jsx
@@ -1,24 +1,37 @@
 import React from 'react'
+import { downloadImage } from '../../services/downloadImage'
 
-const ButtonsForm = () => {
+const ButtonsForm = ({ image, name }) => {
   return (
-    <div className=' flex gap-3 w-full mt-2'>
+    <div className=' flex flex-col gap-3 w-full mt-2'>
+      <div className=' flex gap-3 w-full mt-2'>
+
+        <button
+          className='
+        w-full bg-[#fbed21] text-slate-900 p-1 rounded-lg font-bold
+        hover:scale-105 hover:brightness-200 hover:shadow-2xl ease-in-out duration-300
+            '
+        >
+          Apply
+        </button>
+        <button
+          type='reset'
+          className='
+        w-full bg-[#fbed21] text-slate-900 p-1 rounded-lg font-bold
+              hover:scale-105 hover:brightness-200 hover:shadow-2xl ease-in-out duration-300
+              '
+        >
+          reset
+        </button>
+      </div>
       <button
         className='
-              w-full bg-[#fbed21] text-slate-900 p-1 rounded-lg font-bold
+              w-full bg-[#fbed21] text-slate-900 p-1 rounded-lg font-bold text-center
               hover:scale-105 hover:brightness-200 hover:shadow-2xl ease-in-out duration-300
             '
-      >
-        Apply
-      </button>
-      <button
-        type='reset'
-        className='
-              w-full bg-[#fbed21] text-slate-900 p-1 rounded-lg font-bold
-              hover:scale-105 hover:brightness-200 hover:shadow-2xl ease-in-out duration-300
-            '
-      >
-        reset
+        onClick={(e) => { downloadImage(image, name) }}
+        href={image}
+      >Descargar
       </button>
 
     </div>
