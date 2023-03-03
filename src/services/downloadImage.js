d62af7caa13fa19b321ec412322889cd14b5252a
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-03-03 16:02:41 +0100

    add: downloadImage

---
+++ src/services/downloadImage.js
@@ -0,0 +1,17 @@
+export async function downloadImage (image, name) {
+  await fetch(image)
+    .then((response) => {
+      console.log({ response })
+      return response.blob()
+    })
+    .then((blob) => {
+      const urlBlob = window.URL.createObjectURL(blob)
+      const downloadLink = document.createElement('a')
+      downloadLink.href = urlBlob
+      downloadLink.download = name
+      document.body.appendChild(downloadLink)
+      downloadLink.click()
+      document.body.removeChild(downloadLink)
+      window.URL.revokeObjectURL(urlBlob)
+    })
+}
