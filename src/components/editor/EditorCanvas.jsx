5f7072fd8e298595d9788ef61d74ef3943a0911a
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-03-07 00:59:15 +0100

    add:advertise max size of image

--- src/components/editor/EditorCanvas.jsx
+++ src/components/editor/EditorCanvas.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import ErrorAdvertice from './ErrorAdvertice'
 import ImgEditor from './ImgEditor'
 
 const EditorCanvas = ({ image, originalImg, editedImg, setError }) => {
   return (
     <section className={`flex justify-center items-center w-[${image.width}px] max-h-[${image.height}px] bg-gray-700/50 rounded-3xl border-2 border-[#fbed21] overflow-hidden`}>
       {
-      image ? <ImgEditor img={originalImg} editedImg={editedImg} setError={setError} /> : <h2>No hay imagen disponible</h2>
-    }
+        (image.width > 4000 || image.height > 4000) && <ErrorAdvertice />
+      }
+      {
+        image ? <ImgEditor img={originalImg} editedImg={editedImg} setError={setError} /> : <h2>No hay imagen disponible</h2>
+      }
     </section>
   )
 }
--- src/components/editor/ErrorAdvertice.jsx
+++ src/components/editor/ErrorAdvertice.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 
 const ErrorAdvertice = () => {
   return (
-    <div className='absolute flex gap-4 top-12 bg-amber-500 text-slate-800 py-2 px-4 rounded-lg shadow-lg'>
+    <div className='z-10 absolute flex gap-4 bottom-12 bg-amber-500 text-slate-800 py-2 px-4 rounded-lg shadow-lg'>
       <img className='w-10' src='./static_img/icons/iconAdvertice.svg' alt='icon advertice' />
       <div>
-
-        <p className='text-center'>An error occurred while using Remove Background</p>
-        <p className='text-center'>Please refresh the page and try again.</p>
+        <p className='text-center'>The image cannot be larger than 4000 x 4000 pixels,</p>
+        <p className='text-center'>reduce the size in this app and press apply to see your image</p>
       </div>
     </div>
   )
