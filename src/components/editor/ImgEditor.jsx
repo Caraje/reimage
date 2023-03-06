918317409d950f0a6b044bdfd3ca8e113e5c436b
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-03-06 14:19:39 +0100

    fix: scale img en editor canvas

--- src/components/editor/ImgEditor.jsx
+++ src/components/editor/ImgEditor.jsx
@@ -4,15 +4,15 @@ const ImgEditor = ({ img, editedImg, setError }) => {
   return (
     <>
       <two-up className='flex justify-center items-center'>
-        <div className='max-h-[50rem] max-w-[70rem]'>
+        <div className='max-h-[50rem] max-w-[70rem] object-contain'>
 
-          <img src={img} className=' w-full  object-contain object-center' />
+          <img src={img} className='h-full w-full  object-contain object-center' />
         </div>
-        <div className='max-h-[50rem] max-w-[70rem]'>
+        <div className='max-h-[50rem] max-w-[70rem] object-contain'>
           <img
             src={editedImg}
             onError={() => { setError(true) }}
-            className=' w-full object-contain object-center'
+            className='h-full w-full object-contain object-center'
           />
         </div>
       </two-up>
