027a4316ea77049b227c8cdfb450c4a46d1458b7
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-03-06 17:44:46 +0100

    Fix: load images background Remove

--- src/components/editor/ImgEditor.jsx
+++ src/components/editor/ImgEditor.jsx
@@ -1,19 +1,41 @@
+import { useEffect, useState } from 'react'
+
 import 'two-up-element'
+import LoadAdvertice from './LoadAdvertice'
 
 const ImgEditor = ({ img, editedImg, setError }) => {
+  const [tries, setTries] = useState(0)
+  const [isProcessing, setIsProcessing] = useState(true)
+  const [intervalId, setIntervalId] = useState(null)
+
+  useEffect(() => {
+    if (isProcessing) {
+      clearInterval(intervalId)
+      const newIntervalId = setInterval(() => {
+        setTries((prevTries) => prevTries + 1)
+      }, 500)
+      setIntervalId(newIntervalId)
+    }
+  }, [isProcessing])
   return (
     <>
+      {
+          isProcessing && <LoadAdvertice />
+      }
       <two-up className='flex justify-center items-center'>
         <div className='max-h-[50rem] max-w-[70rem] object-contain'>
 
           <img src={img} className='h-full w-full  object-contain object-center' />
         </div>
         <div className='max-h-[50rem] max-w-[70rem] object-contain'>
+
           <img
-            src={editedImg}
-            onError={() => { setError(true) }}
+            src={`${editedImg}?&t=${tries}`}
+            onLoad={() => { setIsProcessing(false) }}
+            onError={() => { setIsProcessing(true) }}
             className='h-full w-full object-contain object-center'
           />
+
         </div>
       </two-up>
     </>
